5cf625edc3d1ce2cbecb3216f8eb0df5c2f4a03d
Daniel Werf <danielw@bytesnap.co.uk>
2025-10-24 23:12:44 +0100

    updates

---
+++ Converters/BasicConverters.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DuplicateDetector.Converters;
+
+public class BoolToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => (bool)value ? Visibility.Visible : Visibility.Collapsed;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => (Visibility)value == Visibility.Visible;
+}
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => !(bool)value;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => !(bool)value;
+}

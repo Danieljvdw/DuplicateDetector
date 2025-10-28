3686bbe78ced8c9eaa00f2e667f03addb2ed715f
Daniel Werf <danielw@bytesnap.co.uk>
2025-10-28 00:29:33 +0000

    add visual representation of unique, delete and keep files

---
+++ Converters/PercentToStarConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DuplicateDetector.Converters;
+
+public class PercentToStarConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double d)
+            return new GridLength(d, GridUnitType.Star);
+        return new GridLength(0, GridUnitType.Star);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}

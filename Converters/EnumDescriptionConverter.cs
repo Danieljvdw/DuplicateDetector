5fcff7701c79e2a96549bd7d9cb2899a95414f94
Daniel van der Werf <daniel_j_v_d_w@hotmail.co.uk>
2025-10-29 22:56:20 +0000

    descriptions for algorithms

---
+++ Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace DuplicateDetector.Converters;
+
+public class EnumDescriptionConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null) return "";
+
+        FieldInfo fi = value.GetType().GetField(value.ToString());
+        var attr = fi.GetCustomAttribute<DescriptionAttribute>();
+        return attr != null ? attr.Description : value.ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
